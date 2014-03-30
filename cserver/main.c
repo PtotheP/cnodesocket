888f700a308450ed394ff252f36a3c4654c57989
Philipp Seitz <seitz.philipp@outlook.com>
2014-03-30 16:35:06 +0200

    fixed disconnection bug

--- cserver/main.c
+++ cserver/main.c
@@ -17,12 +17,14 @@ int messaging(int new_socket);
 int handle(char shandle[]);
 int control(char command[]);
 void *texting(void *pSocket);
+int textingenabled;
 
 int main(int argc, char *argv[]) {
 
     int socket_desc, socket_con, c;
     struct sockaddr_in server, client;
     pthread_t textthread;
+    textingenabled = 1;
 
     //Create socket
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -52,17 +54,21 @@ int main(int argc, char *argv[]) {
         if (socket_con < 0) {
             perror("accept failed");
         }
+        textingenabled = 1;
         puts("Connection accepted");
         puts("*************************************\n");
 
         pthread_create(&textthread, NULL, texting, (void *) &socket_con);
 
         while (messaging(socket_con));
-        
+
+        puts("Messaging over waiting for Thread textthread to be finished");
+        textingenabled = 0;
+
         pthread_join(textthread, NULL);
 
         close(socket_con);
-        
+
         puts("Connection closed");
         puts("*************************************\n");
     }
@@ -119,14 +125,14 @@ void *texting(void *pSocket) {
     counter = 0;
     char message[2000];
 
-    while (1) {
+    while (textingenabled) {
 
         memset(message, 0, 2000);
         snprintf(message, 2000, "%d", counter);
 
         if (write(textsocket, message, strlen(message)) < 0) {
             puts("COULD NOT WRITE");
-            return 0;
+            return NULL;
         }
 
         counter++;
